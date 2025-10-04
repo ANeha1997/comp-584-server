b04bf77a2abfff306ef25bad73f02f021796bbd0
ANEHA1997 <neha.ananthavaram@gmail.com>
2025-10-03 17:50:49 -0700

    10-3-2025

---
+++ comp-584-server/Controllers/SeedController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldModel;
+
+namespace comp_584_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController(Comp584Context context) : ControllerBase
+    {
+       
+        [HttpPost("Countries")]
+        public async Task<ActionResult> PostCountries()
+        {
+           
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+        
+        [HttpPost("Cities")]
+        public async Task<ActionResult> PostCities()
+        {
+
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
